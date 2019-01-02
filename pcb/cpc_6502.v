6d7fe48450d1a84d7673f3466c30cbd01310eb7b
revaldinho <richevans@mac.com>
2019-01-02 19:27:48 +0000

    Housekeeping changes only

--- pcb/cpc_6502.v
+++ pcb/cpc_6502.v
@@ -1,11 +1,27 @@
-// 
 // cpc_6502 netlist
 // 
 // netlister.py format
 // 
-// (c) Revaldinho, 2018
-// 
-//  
+// This code is part of the z80tube project for interfacing RaspberryPI or Acorn Second Processors
+// to an Amstrad CPC computer                                                                     
+//                                                                                                
+// https://github.com/revaldinho/z80tube                                                          
+//                                                                                                
+// Copyright (C) 2018 Revaldinho                                                                  
+//                                                                                                
+// This program is free software: you can redistribute it and/or modify                           
+// it under the terms of the GNU General Public License as published by                           
+// the Free Software Foundation, either version 3 of the License, or                              
+// (at your option) any later version.                                                            
+//                                                                                                
+// This program is distributed in the hope that it will be useful,                                
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                                 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  
+// GNU General Public License for more details.                                                   
+//                                                                                                
+// You should have received a copy of the GNU General Public License                              
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.                         
+//
 module cpc_6502 ();
 
   // wire declarations
--- src/z80tube.v
+++ src/z80tube.v
@@ -1,3 +1,26 @@
+/*
+ * This code is part of the z80tube project for interfacing RaspberryPI or Acorn Second Processors
+ * to an Amstrad CPC computer
+ * 
+ * https://github.com/revaldinho/z80tube
+ * 
+ * Copyright (C) 2018 Revaldinho
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 // Reserve ports in range &FC10 - &FC1F
 //
 // PMOD and other CPLD registers start at &FC18
